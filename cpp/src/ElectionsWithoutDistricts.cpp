354a532fcbb45c56e57e7f9f13a1c67b8c605501
Alex Bolboaca <alexboly@gmail.com>
2023-11-02 17:28:06 +0200

    Likely copied all methods pertaining to ElectionsWithoutDistricts

--- cpp/src/ElectionsWithoutDistricts.cpp
+++ cpp/src/ElectionsWithoutDistricts.cpp
@@ -2,6 +2,7 @@
 
 void ElectionsWithoutDistricts::addCandidate(const string &candidate)
 {
+    officialCandidates.push_back(candidate);
     candidates.push_back(candidate);
     votesWithoutDistricts.push_back(0);
 }
@@ -30,3 +31,59 @@ void ElectionsWithoutDistricts::recordVoteForNewCandidateInNoDistrictElections(c
     candidates.push_back(candidate);
     votesWithoutDistricts.push_back(1);
 }
+
+map<string, string> ElectionsWithoutDistricts::resultsWithoutDistricts() const
+{
+    map<string, string> results;
+    int nbVotes = 0;
+    int nullVotes = 0;
+    int blankVotes = 0;
+    int nbValidVotes = 0;
+
+    nbVotes = totalNumberOfVotesForElectionsWithoutDistricts();
+    nbValidVotes = numberOfValidVotesForElectionsWithoutDistricts();
+
+    for (int i = 0; i < votesWithoutDistricts.size(); i++)
+    {
+        float candidatResult = ((float)votesWithoutDistricts[i] * 100) / nbValidVotes;
+        string candidate = candidates[i];
+        if (count(officialCandidates.begin(), officialCandidates.end(), candidate) > 0)
+        {
+            results[candidate] = format(candidatResult);
+        }
+        else
+        {
+            if (candidates[i].size() == 0)
+            {
+                blankVotes += votesWithoutDistricts[i];
+            }
+            else
+            {
+                nullVotes += votesWithoutDistricts[i];
+            }
+        }
+    }
+    results["Blank"] = computeAndFormatVotesPercentageOfCategory(blankVotes, nbVotes);
+    results["Null"] = computeAndFormatVotesPercentageOfCategory(nullVotes, nbVotes);
+    results["Abstention"] = computeAndFormatAbstentionData(nbVotes);
+    return results;
+}
+
+int ElectionsWithoutDistricts::totalNumberOfVotesForElectionsWithoutDistricts() const
+{
+    return accumulate(votesWithoutDistricts.begin(), votesWithoutDistricts.end(), 0);
+}
+
+int ElectionsWithoutDistricts::numberOfValidVotesForElectionsWithoutDistricts() const
+{
+    int nbValidVotes = 0;
+    for (int i = 0; i < officialCandidates.size(); i++)
+    {
+        int index = find(candidates.begin(), candidates.end(), officialCandidates[i]) - candidates.begin();
+        nbValidVotes += votesWithoutDistricts[index];
+    }
+
+    return nbValidVotes;
+}
+
+
--- cpp/src/ElectionsWithoutDistricts.h
+++ cpp/src/ElectionsWithoutDistricts.h
@@ -1,6 +1,10 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,11 +12,12 @@ class ElectionsWithoutDistricts
 {
 private:
     vector<string> candidates;
+	vector<string> officialCandidates;
     vector<int> votesWithoutDistricts;
     map<string, vector<string>> list;
 
 public:
-    ElectionsWithoutDistricts(const map<string, vector<string>> &list) : list(list), votesWithoutDistricts{} {};
+    ElectionsWithoutDistricts(const map<string, vector<string>> &list) : list(list), votesWithoutDistricts{}, officialCandidates{} {};
 
     void addCandidate(const string &);
 
@@ -22,4 +27,39 @@ public:
 
     int totalNumberOfVotesForElectionsWithoutDistricts() const;
     int numberOfValidVotesForElectionsWithoutDistricts() const;
+
+    map<string, string> resultsWithoutDistricts() const;
+
+    string computeAndFormatVotesPercentageOfCategory(const int votesOfACategory, const int totalNumberOfVotes) const
+    {
+        float percentageResult = ((float)votesOfACategory * 100) / totalNumberOfVotes;
+        return format(percentageResult);
+    }
+
+    string computeAndFormatAbstentionData(const int nbVotes) const
+    {
+        vector<vector<string>> values;
+        transform(list.begin(), list.end(), back_inserter(values), [](const auto &val)
+                  { return val.second; });
+
+        vector<int> sizes;
+        transform(values.begin(), values.end(), back_inserter(sizes), [](const auto &v)
+                  { return v.size(); });
+        int nbElectors = accumulate(sizes.begin(), sizes.end(), 0);
+        float abstentionResult = 100 - ((float)nbVotes * 100 / nbElectors);
+        return format(abstentionResult);
+    }
+
+    string format(const auto &param) const
+    {
+        // This function is only needed if std::format is not implemented in your standard library
+        // or if the locale support doesn't allow usage of fr-FR
+        ostringstream s;
+        s << std::fixed << std::setprecision(2) << param << "%";
+        string result = s.str();
+        // HACK: This is a hack to simulate the fr-FR locale
+        // We know that only numbers need to be formatted with "," instead of ".", so just replace them
+        replace(result.begin(), result.end(), '.', ',');
+        return result;
+    };
 };
