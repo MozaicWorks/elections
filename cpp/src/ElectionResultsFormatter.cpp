6e135955dc20229a7c8587260ee7c70f7642d83d
Alex Bolboaca <alexboly@gmail.com>
2023-11-07 16:20:25 +0200

    Rename to clarify

--- cpp/src/ElectionResultsFormatter.cpp
+++ cpp/src/ElectionResultsFormatter.cpp
@@ -21,8 +21,8 @@ int ElectionResultsFormatter::countElectors(const map<string, vector<string>> &e
     transform(electorsByDistrict.begin(), electorsByDistrict.end(), back_inserter(electors), [](const auto &val)
               { return val.second; });
 
-    vector<int> sizes;
-    transform(electors.begin(), electors.end(), back_inserter(sizes), [](const auto &v)
+    vector<int> electorCountsPerDistrict;
+    transform(electors.begin(), electors.end(), back_inserter(electorCountsPerDistrict), [](const auto &v)
               { return v.size(); });
-    return accumulate(sizes.begin(), sizes.end(), 0);
+    return accumulate(electorCountsPerDistrict.begin(), electorCountsPerDistrict.end(), 0);
 }
